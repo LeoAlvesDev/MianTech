bd83786cf329c561b36692ada2073530327af7d3
LeoAlvesDev <126263762+LeoAlvesDev@users.noreply.github.com>
2025-02-26 16:20:27 -0300

    Add files via upload

--- about.js
+++ about.js
@@ -2,12 +2,12 @@
 particlesJS("particles-js", {
     particles: {
         number: { value: 80, density: { enable: true, value_area: 800 } },
-        color: { value: "#42a5d6" },
+        color: { value: "#ffffff" },
         shape: { type: "circle" },
         opacity: { value: 0.5, random: true },
         size: { value: 3, random: true },
-        line_linked: { enable: true, distance: 150, color: "#42a5d6", opacity: 0.4, width: 1 },
-        move: { enable: true, speed: 3, direction: "none", random: true, straight: false, out_mode: "out" }
+        line_linked: { enable: true, distance: 150, color: "#747474", opacity: 0.4, width: 1 },
+        move: { enable: true, speed: 0.3, direction: "none", random: true, straight: false, out_mode: "out" }
     },
     interactivity: {
         detect_on: "canvas",
